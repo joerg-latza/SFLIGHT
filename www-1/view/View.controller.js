1a18352d07971f9cbb03a8228d95ccdfd641a63c
Dominik Tornow <dominik.tornow@sap.com>
2013-03-16 19:59:49 -0400

    add service call

--- www-1/view/View.controller.js
+++ www-1/view/View.controller.js
@@ -1,19 +1,45 @@
-V   jQuery.sap.declare({modName:"untitledproject.view.View", type:"controller"});
+jQuery.sap.declare({modName:"untitledproject.view.View", type:"controller"});
 
 jQuery.sap.require("sap.ui.model.odata.datajs");
 
 untitledproject.view.View = function () {
-	   sap.ui.core.mvc.Controller.apply(this, arguments);
-	};
+    sap.ui.core.mvc.Controller.apply(this, arguments);
+};
 
-	untitledproject.view.View.prototype = jQuery.sap.newObject(sap.ui.core.mvc.Controller.prototype);
+untitledproject.view.View.prototype = jQuery.sap.newObject(sap.ui.core.mvc.Controller.prototype);
 
-	/**
-	* Called when a controller is instantiated and its View controls (if available) are already created.
-	* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-	*/
-	untitledproject.view.View.prototype.onInit = function() {
-        alert("Loaded");
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+*/
+untitledproject.view.View.prototype.onInit = function() {
+    jQuery.get("/sflight/sflight/app/hungry.xsjs/count", function(data) {
+        if(data) {
+            this.changeStateToYes();
+        } else {
+            this.changeStateToNo();
+        }
+    });
+};
+
+untitledproject.view.View.prototype.onAfterRendering = function() {
+   this.changeStateToNo();
+};
+
+untitledproject.view.View.prototype.changeStateToYes = function() {
+   this.byId("btnYes").setEnabled(true);
+   this.byId("btnNo").setEnabled(false);
+   this.byId("YesDescription").setVisible(false);
+};
+
+untitledproject.view.View.prototype.changeStateToNo = function() {
+   this.byId("btnNo").setEnabled(true);
+   this.byId("btnYes").setEnabled(false);
+   this.byId("YesDescription").setVisible(true);
+};
+
+untitledproject.view.View.prototype.onConfirm = function() {
+    jQuery.get("/sflight/sflight/app/hungry.xsjs/toggle", function(data) {
         jQuery.get("/sflight/sflight/app/hungry.xsjs/count", function(data) {
             alert(data);
             if(data) {
@@ -22,33 +48,5 @@ untitledproject.view.View = function () {
                 this.changeStateToNo();
             }
         })
-	   };
-	   
-	   untitledproject.view.View.prototype.onAfterRendering = function() {
-		   this.changeStateToNo();
-	   };
-	   
-	   untitledproject.view.View.prototype.changeStateToYes = function() {
-		   this.byId("btnYes").setEnabled(true);
-		   this.byId("btnNo").setEnabled(false);
-		   this.byId("YesDescription").setVisible(false);
-	   };
-	  
-	   untitledproject.view.View.prototype.changeStateToNo = function() {
-		   this.byId("btnNo").setEnabled(true);
-		   this.byId("btnYes").setEnabled(false);
-		   this.byId("YesDescription").setVisible(true);
-	   };
-	   
-	   untitledproject.view.View.prototype.onConfirm = function() {
-        jQuery.get("/sflight/sflight/app/hungry.xsjs/toggle", function(data) {
-        jQuery.get("/sflight/sflight/app/hungry.xsjs/count", function(data) {
-            alert(data);
-            if(data) {
-                this.changeStateToYes();
-            } else {
-                this.changeStateToNo();
-            }
-        })
-        });
-	   };
+    });
+};
